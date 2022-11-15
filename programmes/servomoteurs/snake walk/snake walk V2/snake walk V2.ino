fdbb1b281a5ce4e89ebfbaf37579936469808b98
Younousse Soufiani <soufiani08@live.fr>
2022-11-15 22:17:05 +0100

    Travail Polysnake

---
+++ programmes/servomoteurs/snake walk/snake walk V2/snake walk V2.ino
@@ -0,0 +1,23 @@
+Servo servo0;
+Servo servo2;
+Servo servo3;
+int tet;
+void setup() {
+Serial.begin(9600);
+  servo0.attach(3);
+  servo2.attach(5);
+  servo3.attach(7);
+  servo.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  delay(3000);
+
+}
+
+void loop() {
+  
+
+for(int i=0;i>3;i++);
+
+
+}
